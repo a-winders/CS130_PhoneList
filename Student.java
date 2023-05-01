7898e3d1d6f5cd6fa9e228f8a33449956633fbd0
a-winders <awinders@bellarmine.edu>
2023-05-01 17:56:14 -0400

    Created Student user input

--- Student.java
+++ Student.java
@@ -2,4 +2,16 @@ import java.util.Scanner;
 
 public class Student {
 
+    public static void main(String [] args) {
+
+          // Creates Scanner object 
+    Scanner scan = new Scanner(System.in);
+
+    // Creates new String array
+    String [5] arr;
+
+    System.out.println("Please enter your first name: ");
+    }
+
+  
 }
