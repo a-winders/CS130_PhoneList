8fe941d8b8a44235bd07c60bb26312279f05359d
a-winders <awinders@bellarmine.edu>
2023-05-01 20:48:27 -0400

    Added new color theme

--- Student.py
+++ Student.py
@@ -1,10 +1,17 @@
+# Creates array to hold Student info
+Students = []
+
+# Adds first name to Students array
 firstName = input("Please enter your first name: ")
 print("First name is: " +firstName)
 
+# Adds last name to Students array
 lastName = input("Please enter your last name: ")
 print("Last name is: " +lastName)
 
+# Adds GPA to Students array
 GPA = input("Please enter your GPA: ")
 print("GPA is: "+GPA)
 
+# Prints all info for Student
 print("Full name and GPA is: " +firstName+ " " +lastName+ ", " +GPA+ " GPA")
