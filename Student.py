12dcc5567730ab41983e3a0407d5c9facc1f5c56
a-winders <awinders@bellarmine.edu>
2023-05-01 22:10:35 -0400

    Changed "array" to "list" in comments

--- Student.py
+++ Student.py
@@ -1,15 +1,15 @@
-# Creates array to hold Student info
+# Creates list to hold Student info
 Students = []
 
-# Adds first name to Students array
+# Adds first name to Students list
 firstName = input("Please enter your first name: ")
 print("First name is: " +firstName)
 
-# Adds last name to Students array
+# Adds last name to Students list
 lastName = input("Please enter your last name: ")
 print("Last name is: " +lastName)
 
-# Adds GPA to Students array
+# Adds GPA to Students list
 GPA = input("Please enter your GPA: ")
 print("GPA is: "+GPA)
 
